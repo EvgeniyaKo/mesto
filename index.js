25433e51f818b93f4729021a46ee2efd8b610551
EvgeniyaKo <eriss.retouch@gmailcom>
2022-09-06 22:08:35 +0300

    JS и удаление рамки

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const popup = document.querySelector('.popup');
-const popupOpen = document.querySelector('.popup__open');
-const editButton = document.querySelector('.edit-button');
+const popup = document.querySelector(".popup");
+const popupOpen = document.querySelector(".popup__open");
+const popupClose = document.querySelector(".popup__close");
+const formElement = document.querySelector(".popup__form");
+const inputName = document.querySelector(".input-name");
+const inputJob = document.querySelector(".input-job");
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector(".profile__job");
+const popupButton = document.querySelector(".popup__button");
 
-editButton.addEventListener('click', function(e){
-    e.preventDefault();
-    popup.classList.add('active');
-})
+popupOpen.addEventListener("click", function () {
+  popup.classList.add("active");
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+});
+popupClose.addEventListener("click", function () {
+  popup.classList.remove("active");
+});
+popupButton.addEventListener("click", function () {
+  popup.classList.remove("active");
+});
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+}
+formElement.addEventListener("submit", formSubmitHandler);
