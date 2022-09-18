aeeb19194e3a7d6c44e1101734bc85f37857c903
EvgeniyaKo <eriss.retouch@gmailcom>
2022-09-18 16:00:56 +0300

    третий заход

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,28 @@
 const popup = document.querySelector(".popup");
-const buttonOpenPopup = document.querySelector(".profile__button-edit");
+const popupOpenButton = document.querySelector(".profile__button-edit");
 const popupClose = document.querySelector(".popup__button-close");
 const formElement = document.querySelector(".popup__form");
 const inputName = document.querySelector(".popup__input_name_name");
 const inputJob = document.querySelector(".popup__input_name_job");
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
-const popupButton = document.querySelector(".popup__button-save");
+const popupSave = document.querySelector(".popup__button-save");
 
-buttonOpenPopup.addEventListener("click", function () {
+let openPopup = function () {
   popup.classList.add("popup_opened");
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
-});
-popupClose.addEventListener("click", function () {
+};
+let closePopup = function () {
   popup.classList.remove("popup_opened");
-});
-
+};
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
-  popup.classList.remove("popup_opened");
+  closePopup();
 }
+
+popupOpenButton.addEventListener("click", openPopup);
+popupClose.addEventListener("click", closePopup);
 formElement.addEventListener("submit", formSubmitHandler);
